a80ee229e37004ea6a4072f18ea868658b8d17b8
achintyamishra01 <achintyamishra01@gmail.com>
2022-11-13 07:48:38 +0530

    "sync perfect"

--- src/components/Homepage/homepage.js
+++ src/components/Homepage/homepage.js
@@ -4,7 +4,7 @@ import Masthead from "../Masthead";
 import Footer from "../Footer";
 import About from "../About";
 import '../../app.css'
-import NewsCard from '../Newscard';
+import NewsCard from '../NewsCard';
 
 function homepage() {
   return (
